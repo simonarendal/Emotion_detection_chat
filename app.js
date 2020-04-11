ff2074fbd0bed191943aa5c53c619d39f79fa2f0
Univertigo <lebr@itu.dk>
2020-04-11 11:54:05 +0200

    dots displays when happy is detected
    Co-authored-by: simonarendal <simonarendal@gmail.com>

--- app.js
+++ app.js
@@ -117,8 +117,6 @@ io.sockets.on('connection',
 /////////////////////////////////////////////////////////////
 */
 
-
-
 //////////////////////MQTT/////////////////////////////////////
 
 /*
@@ -145,13 +143,17 @@ io.sockets.on('connection',
 
 // We generate a random unique clientId when we start
 // MQTT sends messages to everyone, including ourselves, so this is a way to make sure we recognize our own messages
+
+let ellipses = [];
+
+
 var clientOptions = {
-	clientId : 'mqttjs_' + Math.random().toString(16).substr(2, 8)
+  clientId: "mqttjs_" + Math.random().toString(16).substr(2, 8),
 };
 
 // Uncomment one of the three following lines to choose your broker
 // var MQTTBrokerUrl = 'ws://iot.eclipse.org:80/ws';
-var MQTTBrokerUrl = 'wss://test.mosquitto.org:8081';
+var MQTTBrokerUrl = "wss://test.mosquitto.org:8081";
 // var MQTTBrokerUrl = 'ws://broker.hivemq.com:8000';
 
 // We connect to it in the beginning
@@ -162,197 +164,163 @@ var client = mqtt.connect(MQTTBrokerUrl, clientOptions);
 // Topics can be thought of like "chat rooms", only those listening to the correct topic get the message
 // Make sure that this is very unique, so you only get your own messages
 // I.e. don't name it 'test', but instead 'JesperHyldahlFoghTest'
-// 
-var Topic = 'LesiEmotionChat'; // CHANGE THIS TO SOMETHING UNIQUE TO YOUR PROJECT
+//
+var Topic = "LesiEmotionChat"; // CHANGE THIS TO SOMETHING UNIQUE TO YOUR PROJECT
+var connectTopic = Topic + "-connect";
+var happyTopic = Topic + "-happy";
 
 // We use a timer in order to only show the interface after we are fairly certain we have a unique ID
 var connectionTimer = null;
 
-
 // When we connect we want to do something
-client.on('connect', function (connack) {
-	console.log('connected')
+client.on("connect", function (connack) {
+  console.log("connected");
 
-	// Messages are called payloads in MQTT
-	// We create a payload with our unique ID and a textual message
-	var payload = {
-		clientId : clientOptions.clientId,
-		message : 'HELLO'
-	};
-	// We send/publish the message to the topic
-	client.publish(Topic, JSON.stringify(payload));
+  // Messages are called payloads in MQTT
+  // We create a payload with our unique ID and a textual message
+  var payload = {
+    clientId: clientOptions.clientId,
+    message: "HELLO",
+  };
+  // We send/publish the message to the topic
+  client.publish(connectTopic, JSON.stringify(payload));
 
-	// Update the interface
-	//updateTimer();
-
-	// We start subscribing/listening to the topic
-  	client.subscribe(Topic, function(err) {
+  client.subscribe(connectTopic, function (err) {
     // If we get an error show it on the console so we can see what went wrong
-    if(err) {
-    	console.log('connection error');
+    if (err) {
+      console.log("connection error");
     }
-  })
+  });
 
-})
+  client.subscribe(happyTopic, function (err) {
+    // If we get an error show it on the console so we can see what went wrong
+    if (err) {
+      console.log("connection error");
+    }
+  });
+});
 
 // When we get any kind of message, we want to do something
-client.on('message', function (topic, payload) {
+client.on("message", function (topic, payload) {
+  console.log("received message");
 
-	// The payload comes in as a Buffer(i.e. incomprehensible bytes), so we need to convert it first
-	// This happens by using JSON.parse() after converting the Buffer to a string
-	var convertedPayload = JSON.parse(payload.toString());
+  // The payload comes in as a Buffer(i.e. incomprehensible bytes), so we need to convert it first
+  // This happens by using JSON.parse() after converting the Buffer to a string
+  var convertedPayload = JSON.parse(payload.toString());
 
-	if(convertedPayload.message === 'HELLO'){
-		console.log('other client joined' );
-	}
+  // If we got a payload on the connection topic, do this
+  if (topic === connectTopic) {
+    // If we got a payload from someone else than us, and they have the same ID as us
+    if (convertedPayload.clientId !== clientOptions.clientId) {
+      // We get this message if someone has already claimed this ID
+      if (convertedPayload.message === "HELLO") {
+        console.log("other client joined on connectTopic");
 
-/*
-	// If we got a payload on the connection topic, do this
-	if(topic === connectTopic) {
+       
+      }
+    }
+  }
 
-		// If we got a payload from someone else than us, and they have the same ID as us
-		if(convertedPayload.clientId !== clientOptions.clientId && convertedPayload.id === numericId) {
-			// We get this message if someone has already claimed this ID
-			if(convertedPayload.message === 'ID_TAKEN') {
-				// Increase our ID by one
-				numericId++;
-
-				// We say hello again with our new ID
-				var helloPayload = {
-					id : numericId,
-					clientId : clientOptions.clientId,
-					message : 'HELLO'
-				};
-				// We send/publish the payload to the connection topic
-				client.publish(connectTopic, JSON.stringify(helloPayload));
-
-				// Restart the connection timer
-				updateTimer();
-
-				// Show on the interface that we did not get 
-				$('.dot-container span').text('Id ' + (numericId - 1) + ' was taken. Looking some more');
-			}
-			// We get this message if someone new comes along, or someone has gotten a new ID
-			else if(convertedPayload.message === 'HELLO') {
-				// Let everyone know that this ID is taken
-				var changePayload = {
-					id : numericId,
-					clientId : clientOptions.clientId,
-					message : 'ID_TAKEN'
-				};
-				// We send/publish the payload to the connection topic
-				client.publish(connectTopic, JSON.stringify(changePayload));
-			}
-		}
-	}
-	else if(topic === firstTopic) {
-		// If we get a message on the first topic, vibrate the phone
-		if(convertedPayload.clientId !== clientOptions.clientId) {
-      //navigator.vibrate(500);
-      console.log('test payload received - other client joined')
-			// And after five seconds
-			setTimeout(function() {
-				// Let everyone know that you finished vibrating
-				var vibratePayload = {
-					id : numericId,
-					clientId : clientOptions.clientId,
-					message : 'VIBRATION_DONE'
-				};
-				// We send/publish the payload to the connection topic
-				client.publish(secondTopic, JSON.stringify(vibratePayload));
-			}, 500)
-		}
-	}
-	else if(topic === secondTopic) {
-		// If we get a message on the second topic, show where we got it from
-		if(convertedPayload.clientId !== clientOptions.clientId) {
-			$('.vibration-status').text('Client #' + convertedPayload.id + ' is done vibrating.');
-		}
-	}*/
-})
+  if (topic === happyTopic) {
+    if (convertedPayload.message === "HAPPY") {
+      console.log('received "HAPPY"');
+	  happyCounter++;
+	  ellipses.push(new Ellipse());
+	 //drawEllipse()
+      console.log("happyCounter: " + happyCounter);
+    }
+  }
+});
 
 // Sets a timer that updates the interface, if enough time has passed
 // The alternative would be to update the interface every time we are checking out a new ID
-/*
-function updateTimer() {
-	// If we already set a timer, clear it
-	if(connectionTimer !== null) clearTimeout(connectionTimer);
-	// Update the interface, if we haven't received another ID_TAKEN message within 5000 milliseconds
-	//connectionTimer = setTimeout(updateConnectionStatus, 5000);
-}
-*/
 
-/*
+function updateTimer() {
+  // If we already set a timer, clear it
+  if (connectionTimer !== null) clearTimeout(connectionTimer);
+  // Update the interface, if we haven't received another ID_TAKEN message within 5000 milliseconds
+  connectionTimer = setTimeout(updateConnectionStatus, 5000);
+}
+
 // Show the current connection status to the page
 function updateConnectionStatus() {
-	// Hide, show and update the appropriate 
-	//$('.connecting').hide();
-	//$('.message-container h3 ').show();
-	// By changing the attribute 'data-id' on the HTML body, we can change the styling. See the CSS file for more.
-	//$('body').attr('data-id', numericId);
-	//$('.id-elem').text(numericId);
-
-	
+  // Hide, show and update the appropriate
+  //$('.connecting').hide();
+  //$('.message-container h3 ').show();
+  // By changing the attribute 'data-id' on the HTML body, we can change the styling. See the CSS file for more.
+  //$('body').attr('data-id', numericId);
+  //$('.id-elem').text(numericId);
 }
-*/
-//////////////////////MQTT/////////////////////////////////////
+  ///////////////////////////////////////////////////////////////
+ /////////////////////TOKBOX////////////////////////////////////
+///////////////////////////////////////////////////////////////
 
 // replace these values with those generated in your TokBox Account
 var apiKey = "46651242";
-var sessionId = "2_MX40NjY1MTI0Mn5-MTU4NjE2NTg3NTI2MH5SaHR4amgvNlRJSHVyNzFWYXEweTh2eWN-fg";
-var token = "T1==cGFydG5lcl9pZD00NjY1MTI0MiZzaWc9NWE3YmQxNTg4MTkxZGY1YTNjNmMxMDE3MWQyMTY1NGQ2ZmEzNDQ4YTpzZXNzaW9uX2lkPTJfTVg0ME5qWTFNVEkwTW41LU1UVTROakUyTlRnM05USTJNSDVTYUhSNGFtZ3ZObFJKU0hWeU56RldZWEV3ZVRoMmVXTi1mZyZjcmVhdGVfdGltZT0xNTg2MTY3MzM5Jm5vbmNlPTAuOTQ0MjQ1OTcxNTk0NTU3JnJvbGU9c3Vic2NyaWJlciZleHBpcmVfdGltZT0xNTg2MTcwOTM4JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
-
+var sessionId =
+  "2_MX40NjY1MTI0Mn5-MTU4NjE2NTg3NTI2MH5SaHR4amgvNlRJSHVyNzFWYXEweTh2eWN-fg";
+var token =
+  "T1==cGFydG5lcl9pZD00NjY1MTI0MiZzaWc9NWE3YmQxNTg4MTkxZGY1YTNjNmMxMDE3MWQyMTY1NGQ2ZmEzNDQ4YTpzZXNzaW9uX2lkPTJfTVg0ME5qWTFNVEkwTW41LU1UVTROakUyTlRnM05USTJNSDVTYUhSNGFtZ3ZObFJKU0hWeU56RldZWEV3ZVRoMmVXTi1mZyZjcmVhdGVfdGltZT0xNTg2MTY3MzM5Jm5vbmNlPTAuOTQ0MjQ1OTcxNTk0NTU3JnJvbGU9c3Vic2NyaWJlciZleHBpcmVfdGltZT0xNTg2MTcwOTM4JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
 
 // Handling all of our errors here by alerting them
 function handleError(error) {
-    if (error) {
-      alert(error.message);
-    }
+  if (error) {
+    alert(error.message);
   }
-
+}
 
 // (optional) add server code here
 // This connects to our heroku-app that serves the clients with sessions and tokens
-var SERVER_BASE_URL = 'https://emotion-detection-01.herokuapp.com';
-fetch(SERVER_BASE_URL + '/session').then(function(res) {
-  return res.json()
-}).then(function(res) {
-  apiKey = res.apiKey;
-  sessionId = res.sessionId;
-  token = res.token;
-  initializeSession();
-}).catch(handleError);
+var SERVER_BASE_URL = "https://emotion-detection-01.herokuapp.com";
+fetch(SERVER_BASE_URL + "/session")
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (res) {
+    apiKey = res.apiKey;
+    sessionId = res.sessionId;
+    token = res.token;
+    initializeSession();
+  })
+  .catch(handleError);
 
 // We initialize a session with our tokbox api key, and the session Id created by the heroku-app
 function initializeSession() {
-    var session = OT.initSession(apiKey, sessionId);
-    
-    // We want clients to be able to subscribe to (or view) each other's streams in the session.
-    // Subscribe to a newly created stream
-    session.on('streamCreated', function(event) {
-        session.subscribe(event.stream, 'subscriber', {
-          insertMode: 'append',
-          width: '100%',
-          height: '100%'
-        }, handleError);
-      });
-      
-  
-    // Create a publisher so the clients send/publish their webcam stream
-    var publisher = OT.initPublisher('publisher', {
-      insertMode: 'append',
-      width: '100%',
-      height: '100%'
-    }, handleError);
-  
-    // Connect to the session 
-    session.connect(token, function(error) {
-      // If the connection is successful, publish to the session
-      if (error) {
-        handleError(error);
-      } else {
-        session.publish(publisher, handleError);
-      }
-    });
-  }
-  
+  var session = OT.initSession(apiKey, sessionId);
+
+  // We want clients to be able to subscribe to (or view) each other's streams in the session.
+  // Subscribe to a newly created stream
+  session.on("streamCreated", function (event) {
+    session.subscribe(
+      event.stream,
+      "subscriber",
+      {
+        insertMode: "append",
+        width: "100%",
+        height: "100%",
+      },
+      handleError
+    );
+  });
+
+  // Create a publisher so the clients send/publish their webcam stream
+  var publisher = OT.initPublisher(
+    "publisher",
+    {
+      insertMode: "append",
+      width: "100%",
+      height: "100%",
+    },
+    handleError
+  );
+
+  // Connect to the session
+  session.connect(token, function (error) {
+    // If the connection is successful, publish to the session
+    if (error) {
+      handleError(error);
+    } else {
+      session.publish(publisher, handleError);
+    }
+  });
+}
--- emotion_detection/sketch.js
+++ emotion_detection/sketch.js
@@ -1,96 +1,82 @@
-//var socket;
 var happyCounter = 0;
-var happyTreshold = 0.2;
+var localHappyCounter = 0;
+var happyTreshold = 0.3;
+//let ellipses = [];
+let e;
+
 function setup() {
     loadCamera();
     loadTracker();
     loadCanvas(windowWidth,windowHeight);
-    
-    /*
-    // From our 'webchat' sketch
-    socket = io.connect('https://emotion-detection-01.herokuapp.com');
-    socket.on('mouse', newDrawing);
-    */
+    //e = new Ellipse(random(0,width), random(0, 100), random(100,255), 0, random(100,255));
 }
-      
-function draw() {
 
-    getPositions();
+function draw() {
+    //e.createEllipse();
+    //getPositions();
     getEmotions();
     clear();
     
     videoInput.hide();
 
+    for(var i =0; i< ellipses.length; i++){
+    ellipses[i].createEllipse();
+    }
+
     noStroke();
     fill(0,150);
     //rect(0,0,width,height);
 
-    
-/*
     if (emotions) {
-   
-
         // angry=0, sad=1, surprised=2, happy=3
         for (var i = 0;i < predictedEmotions.length;i++) {
             rect(i * 250+20, 120, 30, -predictedEmotions[i].value * 30);    
-        
-            if (predictedEmotions[3].value < happyTreshold){
-                var notHappyPayload = {
-                    clientId : clientOptions.clientId,
-                    message : 'NOT_HAPPY'
-                };
-                // We send/publish the payload to the connection topic
-                client.publish(Topic, JSON.stringify(notHappyPayload));
+            text('happyCounter=' + str(happyCounter),60, width/2);
+                     
 
-                // ANd writes in the log
-                //console.log('sending "NOT_HAPPY"')
-                
-        
-                // Restart the connection timer
-                updateTimer();
+            if (predictedEmotions[3].value >= happyTreshold){
+                localHappyCounter ++;
+                console.log('localHappyCounter =' + str(localHappyCounter))
             }
-           
-
-            if (predictedEmotions[3].value > happyTreshold){
-                var HappyPayload = {
-                    clientId : clientOptions.clientId,
-                    message : 'HAPPY'
-                };
-                // We send/publish the payload to the connection topic
-                client.publish(Topic, JSON.stringify(HappyPayload));
-
-                // ANd writes in the log
-                //console.log('sending "HAPPY"')
-                
         
                 // Restart the connection timer
                 //updateTimer();
             }
-            //console.log('pev =' + predictedEmotions[3].value);
-
+        
+            
+        if (localHappyCounter >= 100) { 
+                localHappyCounter = 0;
+                
+                //console.log('sending "HAPPY"')
+                // We send/publish the payload to the happy topic
+                publishFunc();
+                
         }
-    }
-  
-
-    client.on('message', function (topic, payload) {
-        var convertedPayload = JSON.parse(payload.toString());
-
-        if(convertedPayload.message === 'HAPPY') {
-            happyCounter ++; 
         }
-        console.log('happyCounter: ' + str(happyCounter));
 
-    })
-  
-    
-
-    text("ANGRY", 20, 140);
-    text("SAD", 270, 140);
-    text("SURPRISED", 520, 140);
+ 
     text("HAPPY", 770, 140);
     
-    */
-}
 
+
+    
+   }
+
+   function publishFunc () {
+    var HappyPayload = {
+        clientId : clientOptions.clientId,
+        message : 'HAPPY'                     
+    };
+    client.publish(happyTopic, JSON.stringify(HappyPayload));
+    console.log('message published')
+
+}    
+
+function drawEllipse() {
+    console.log('drawing ellipse')
+    //e.createEllipse();
+    fill(0,0,0);
+    ellipse(random(0, width), 20, 20, 20);
+}
 
 
