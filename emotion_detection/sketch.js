170adbed65cf8d140abfce4d9a40453ccf24405e
simonarendal <simonarendal@gmail.com>
2020-04-14 09:54:39 +0200

    reduced size of subsriber window

--- emotion_detection/sketch.js
+++ emotion_detection/sketch.js
@@ -1,41 +1,35 @@
 var happyCounter = 50;
 var localHappyCounter = 0;
 var happyTreshold = 0.2;
-//let ellipses = [];
-//let e;
+
 
 function setup() {
     loadCamera();
     loadTracker();
     loadCanvas(windowWidth,windowHeight);
     videoInput.hide();
-
-    //e = new Ellipse(random(0,width), random(0, 100), random(100,255), 0, random(100,255));
 }
 
-function draw() {
 
-    //e.createEllipse();
-    //getPositions();
+
+
+function draw() {
+    
+        
+
+ 
     getEmotions();
     clear();
-    if(happyCounter  > 0){happyCounter -=0.2;}
+    if(happyCounter  > 0){happyCounter -=0.1;}
     console.log('happyCounter' + str(happyCounter));
-    //fill(0,happyCounter,0);
-    //rect(0,0,windowWidth,windowHeight);
     background(120,250,70, round(happyCounter))
 
-    /*
-    for(var i =0; i< ellipses.length; i++){
-    ellipses[i].createEllipse();
-    }
-    */
     
     if (emotions) {
         // angry=0, sad=1, surprised=2, happy=3
         for (var i = 0;i < predictedEmotions.length;i++) {
             //rect(i * 250+20, 120, 30, -predictedEmotions[i].value * 30);    
-            text('happyCounter=' + str(happyCounter),60, width/2);
+            //text('happyCounter=' + str(happyCounter),60, width/2);
                      
 
             if (predictedEmotions[3].value >= happyTreshold){
@@ -52,7 +46,7 @@ function draw() {
             }
         
             
-        if (localHappyCounter >= 60) { 
+        if (localHappyCounter >= 40) { 
                 localHappyCounter = 0;
                 
                 //console.log('sending "HAPPY"')
@@ -73,12 +67,3 @@ function draw() {
     //console.log('message published')
 
 }    
-
-function drawEllipse() {
-    //console.log('drawing ellipse')
-    //e.createEllipse();
-    fill(0,0,0);
-    ellipse(random(0, width), 20, 20, 20);
-}
-
-
