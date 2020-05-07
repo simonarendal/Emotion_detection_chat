083fcd3891ce250451b8f53b2ee16752bbf5e599
Univertigo <57756062+Univertigo@users.noreply.github.com>
2020-05-07 11:49:42 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,13 +3,15 @@ const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 const video = document.getElementById('video');
 var expression;
+var readFace = false;
 
 var roomName = 'green';
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('./fading_background/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('./fading_background/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('./fading_background/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('./green/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('./green/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('./green/models')
 ]).then(startVideo)
+
 
 function startVideo() {
 if (navigator.mediaDevices.getUserMedia) {
@@ -18,10 +20,12 @@ if (navigator.mediaDevices.getUserMedia) {
       video.srcObject = stream;
     })
     .catch(function (err0r) {
-      console.log("Something went wrong!");
+      console.log("Something went wrong with video feed!");
     });
 }
 }
+
+
 
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
@@ -30,47 +34,52 @@ function setup() {
 function draw() {
 background(255,255,255);  
 var expression1 = parseFloat(expression).toFixed(3);
-textSize(20);
+textSize(18);
 textAlign(CENTER,TOP);
 fill(100, 255, 100);
 text('Happy scale:' + (expression1), 0, 0, windowWidth);
 
 fill(0, 0, 0);
 
-text('Login med kodeordet green, når du har læst nedenstående', windowWidth/2,50)
+text('When the clock is 18.30 pm, please login with the password "green"', windowWidth/2,50)
+text('At this point you will only see the other person, while sharing a mutual background!', windowWidth/2,75)
 
 textSize(14);
-text('Husk at trykke "OK", når du bliver spurgt om du tillader, at vi gør brug af webcam og mikrofon', windowWidth/2,80) 
-text('Appen bruger dit webcam til at prøve at se, hvordan jeres date går', windowWidth/2,105) 
-text('Resultatet vil vi præsentere for dig, og sammenligne med din egen oplevelse i det opfølgende interview ', windowWidth/2,130)
-text('Jeres samtale er fuldstændig privat, da vi hverken ser eller hører på jer, mens i er på date', windowWidth/2,155) 
+text('Remember to press "OK", when you are ask if you allow the usage of webcam and microphone', windowWidth/2,105)
+text('Please test if the lighting is good - app tells you if it "cannot read face"', windowWidth/2,130)
+text('Please have a bit of patience, as the app can take some time to load', windowWidth/2,155)  
+text('Ulf will stay on the platform after the date ends. Lif will receive facebook pm when her turn to be interviewed.', windowWidth/2,180)    
+//text('Husk at trykke "OK", når du bliver spurgt om du tillader, at vi gør brug af webcam og mikrofon', windowWidth/2,80) 
+//text('Appen bruger dit webcam til at prøve at se, hvordan jeres date går', windowWidth/2,105) 
+//text('Resultatet vil vi præsentere for dig, og sammenligne med din egen oplevelse i det opfølgende interview ', windowWidth/2,130)
+//text('Jeres samtale er fuldstændig privat, da vi hverken ser eller hører på jer, mens i er på date', windowWidth/2,155) 
+
+if(readFace === false){
+ fill(255,0,0);
+ text('Cannot read face!',windowWidth/2,30);
+  }
+
 }
 
 
-/*
-function startVideo() {
-    navigator.getUserMedia(
-      { video: {} },
-      stream => video.srcObject = stream,
-      err => console.error(err)
-    )
-
-  }
-  */
   video.addEventListener('play', () => {
    // const canvas = faceapi.createCanvasFromMedia(video)
     //document.body.append(canvas)
     //const displaySize = { width: video.width, height: video.height }
     //faceapi.matchDimensions(canvas, displaySize)
     setInterval(async () => {
-      
+    try{
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-   
     expression = (detections[0].expressions.happy);
-
     console.log(detections[0].expressions.happy);
- 
+    readFace = true;
+    }
    
+    catch (err){
+    console.log(err);
+    console.log('something went wrong with face api');
+    readFace = false;
+    }
     //}
       //console.log('localHappyCounter = ' + (localHappyCounter))
 
@@ -87,7 +96,7 @@ loginButton.addEventListener("click", (e) => {
 
   if (password === roomName) {
       // If the credentials are valid, show an alert box and reload the page
-      alert("I agree to the terms");
+      alert("You are being redirected to your date now, have fun!");
       location.replace("https://leonorabryndum.dk/tokbox/" + roomName)  
   } else {
       // Otherwise, make the login error message show (change its oppacity)
